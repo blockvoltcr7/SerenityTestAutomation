64adda41c434739e800e4d62596064bce6d15495
Sami Sabir-Idrissi <sami_sabir-idrissi@epam.com>
2023-04-05 03:22:06 -0400

    successfully launched Edge Browser with WebDriverManager and Selenium version 4 and using gradle 7

--- src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -2,13 +2,28 @@ package starter.stepdefinitions;
 
 
 import io.cucumber.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 
 public class SearchStepDefinitions {
 
+
+
     @Given("My first smoke test")
-    public void smoke() {
+    public void smoke() throws InterruptedException {
+
         System.out.println("hello world");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+        Thread.sleep(10000);
+        driver.close();
+        driver.quit();
+
     }
 
 }
